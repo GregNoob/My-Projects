ec55ec31f40720a02f3571e342deec3a438930c5
George Alex <caplea.george@yahoo.com>
2023-02-07 18:27:40 +0200

    fixed cheerio const

--- web-scraper/index.js
+++ web-scraper/index.js
@@ -1,27 +1,27 @@
-const PORT = 8000; 
+const PORT = 8000;
 
-const axios = require('axios');
-const cheerio = require('cheerio');
-const express = require('express');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
 
 const webScrap = express();
 
-const url = 'https://www.guitarworld.com/';
+const url = "https://www.guitarworld.com/";
 axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = load(html)
-        const articles = []
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
 
-
-        $('.article-name', html).each(function(){
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                url
-            })
-            console.log(articles)
-        })
-    }).catch(err => console.log(err));
+    $(".article-name", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+      console.log(articles);
+    });
+  })
+  .catch((err) => console.log(err));
 webScrap.listen(PORT, () => console.log(`server listening on ${PORT}`));
