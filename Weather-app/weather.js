3d2696bd9dc115d82e45b74c92ab9ec1b6fc1ecf
George Alex <caplea.george@yahoo.com>
2023-07-15 16:10:45 +0300

    fixed weather script

--- Weather-app/weather.js
+++ Weather-app/weather.js
@@ -1,42 +1,43 @@
 // Weather API
-const apiKey = 'ba090421e9e5158d62e374da21b43928';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin';
+const apiKey = "ba090421e9e5158d62e374da21b43928";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-const searchBox = document.querySelector('.search input');
-const searchBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('weather-icon');
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
+async function checkWeather(city) {
+  const response = await fetch(`${apiUrl}?q=${city}&appid=${apiKey}&units=metric`);
+  const data = await response.json();
 
-    console.log(data);
+  console.log(data);
+  // Check if the API response contains valid data
+  if (data.cod === 200) {
     // update data after user input
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + '°C';
-    document.querySelector(".humidity").innerHTML = data.main.humidity + ' %';
-    document.querySelector(".wind").innerHTML = data.wind.speed + ' km/h';
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
     // update image after user input
-    if(data.weather[0].main == 'Clouds'){
-        weatherIcon.scr = "img/clouds.png";
-    }
-    else if (data.weather[0].main == 'Clear'){
-        weatherIcon.scr = "img/clear.png";
-    }
-    else if (data.weather[0].main == 'Rain'){
-        weatherIcon.scr = "img/rain.png";
-    }
-    else if (data.weather[0].main == 'Drizzle'){
-        weatherIcon.scr = "img/drizzle.png";
-    }
-    else if (data.weather[0].main == 'Snow'){
-        weatherIcon.scr = "img/snow.png";
-    }
-};
+    const weatherConditions = {
+      Clouds: "img/clouds.png",
+      Clear: "img/clear.png",
+      Rain: "img/rain.png",
+      Drizzle: "img/drizzle.png",
+      Snow: "img/snow.png"
+    };
 
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
-})
+    const weatherMain = data.weather[0].main;
+    weatherIcon.src = weatherConditions[weatherMain] || "img/clear.png";
+  } else {
+    // Handle the case when the API response is not successful
+    console.error("Error fetching weather data:", data.message);
+  }
+}
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+checkWeather(searchBox.value);
